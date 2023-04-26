f65d113ece79ae20e8ca553b7879e6e9297e665e
baiyang <17801003317@163.com>
2023-04-27 03:00:17 +0800

    熟悉glsl语言

--- app/src/main/java/com/example/nativedemo/TriangleRender.java
+++ app/src/main/java/com/example/nativedemo/TriangleRender.java
@@ -13,6 +13,8 @@ import javax.microedition.khronos.opengles.GL10;
 public class TriangleRender implements GLSurfaceView.Renderer {
     private int mProgram;
     private FloatBuffer vertexBuffer;
+
+    // CPU的数据往vPosition里塞，vPosition再传给GPU的内置变量
     private final String vertexShaderCode =
             "attribute vec4 vPosition; " +
                     "void main() {" +
@@ -25,6 +27,8 @@ public class TriangleRender implements GLSurfaceView.Renderer {
                     "void main() {" +
                     "  gl_FragColor = vColor;" +
                     "}";
+
+    // 顶点形状
     static float triangleCoords[] = {
             0.5f,  0.5f, 0.0f, // top
             -0.5f, -0.5f, 0.0f, // bottom left
@@ -41,26 +45,53 @@ public class TriangleRender implements GLSurfaceView.Renderer {
         GLES20.glCompileShader(shader);
         return shader;
     }
+
+
     @Override
     public void onSurfaceCreated(GL10 gl, EGLConfig config) {
+        // 将背景设置为灰色
         GLES20.glClearColor(0.5f,0.5f,0.5f,1.0f);
-        //申请底层空间
+        // CPU--->GPU 通过ByteBuffer 实现数据从cpu到gpu的转换
         ByteBuffer bb = ByteBuffer.allocateDirect(triangleCoords.length * 4);
+        // GPU重新整理内存
         bb.order(ByteOrder.nativeOrder());
-        //将坐标数据转换为FloatBuffer，用以传入OpenGL ES程序
+
+        // 在GPU中，需要将ByteBuffer转换为FloatBuffer，用以传入OpenGL ES程序
         vertexBuffer = bb.asFloatBuffer();
         vertexBuffer.put(triangleCoords);
         vertexBuffer.position(0);
-        int vertexShader =  loadShader(GLES20.GL_VERTEX_SHADER,vertexShaderCode);
-        int fragmentShader = loadShader(GLES20.GL_FRAGMENT_SHADER,fragmentShaderCode);
-        //创建一个空的OpenGLES程序
+
+        // 1.创建一个顶点程序  2.将顶点程序从CPU传入GPU  3.编译顶点程序
+        int vertexShader = GLES20.glCreateShader(GLES20.GL_VERTEX_SHADER);
+        GLES20.glShaderSource(vertexShader, vertexShaderCode);
+        GLES20.glCompileShader(vertexShader);
+
+        // 1.创建一个片元程序  2.将片元程序从CPU传入GPU  3.编译片元程序
+        int fragmentShader = GLES20.glCreateShader(GLES20.GL_VERTEX_SHADER);
+        GLES20.glShaderSource(fragmentShader, fragmentShaderCode);
+        GLES20.glCompileShader(fragmentShader);
+
+        // 顶点程序、片元程序，本质上都是一个在GPU中运行的可执行程序
         mProgram = GLES20.glCreateProgram();
-        //将顶点着色器加入到程序
-        GLES20.glAttachShader(mProgram,vertexShader);
-        //将片元着色器加入到程序中
-        GLES20.glAttachShader(mProgram,fragmentShader);
-        //连接到着色器程序
+        GLES20.glAttachShader(mProgram, vertexShader);
+        GLES20.glAttachShader(mProgram, fragmentShader);
+
+        // 链接顶点、片元程序，并且生成一个可执行的程序
         GLES20.glLinkProgram(mProgram);
+
+//
+//
+////        int vertexShader =  loadShader(GLES20.GL_VERTEX_SHADER,vertexShaderCode);
+////        int fragmentShader = loadShader(GLES20.GL_FRAGMENT_SHADER,fragmentShaderCode);
+//
+//        //创建一个空的OpenGLES程序
+//        mProgram = GLES20.glCreateProgram();
+//        //将顶点着色器加入到程序
+//        GLES20.glAttachShader(mProgram,vertexShader);
+//        //将片元着色器加入到程序中
+//        GLES20.glAttachShader(mProgram,fragmentShader);
+//        //连接到着色器程序
+//        GLES20.glLinkProgram(mProgram);
     }
 
     @Override
@@ -70,17 +101,16 @@ public class TriangleRender implements GLSurfaceView.Renderer {
 
     private int mPositionHandle;
     private int mColorHandle;
-    //顶点个数
     @Override
     public void onDrawFrame(GL10 gl) {
-        //将程序加入到OpenGLES2.0环境
+        // 将程序加入到OpenGLES2.0环境
         GLES20.glUseProgram(mProgram);
 
-        //获取顶点着色器的vPosition成员句柄
+        // 获取顶点着色器的vPosition成员句柄
         mPositionHandle = GLES20.glGetAttribLocation(mProgram, "vPosition");
-        //启用三角形顶点的句柄
+        // 启用三角形顶点的句柄
         GLES20.glEnableVertexAttribArray(mPositionHandle);
-        //准备三角形的坐标数据
+        // 准备三角形的坐标数据
         GLES20.glVertexAttribPointer(mPositionHandle, 3,
                 GLES20.GL_FLOAT, false,
                 12, vertexBuffer);
